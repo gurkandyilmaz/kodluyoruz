4de4280019c403a48cd9164d679baa06fc29768a
GurkanD Yilmaz <gurkandy@gmail.com>
2021-12-07 23:24:05 +0300

    sql odev 3

--- patikadev/veri_bilimi/sql/sql_scripts.sql
+++ patikadev/veri_bilimi/sql/sql_scripts.sql
@@ -1,5 +1,30 @@
-/*ODEV 2*/
+/*ODEV 4*/
 
+/*ODEV 3*/
+/*
+SELECT * FROM film
+WHERE (title LIKE 'C%') AND (length > 90) AND (rental_rate = 2.99)
+*/
+/*
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t'
+ORDER BY title ASC
+*/
+
+/*
+SELECT * FROM country
+WHERE country LIKE '_____%n'
+*/
+/*ODEV 2*/
+/*
+SELECT * FROM film
+WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99)
+*/
+/*
+SELECT first_name, last_name FROM actor
+WHERE first_name IN ('Penelope', 'Nick', 'Ed')
+ORDER BY first_name ASC
+*/
 /*
 SELECT * from film
 WHERE replacement_cost BETWEEN 12.99 AND 16.98
